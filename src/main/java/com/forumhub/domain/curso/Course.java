7d29a7073cf2ce3359402c1916ca166197983db7
Douglascrc <douglasbuscam@gmail.com>
2025-08-08 23:22:14 -0300

    feat: add Course entity with attributes and JPA annotations

---
+++ src/main/java/com/forumhub/domain/curso/Course.java
@@ -0,0 +1,19 @@
+package com.forumhub.domain.curso;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Course")
+@Table(name = "courses")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Course {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    public Long id;
+    public String name;
+    public String category;
+}
