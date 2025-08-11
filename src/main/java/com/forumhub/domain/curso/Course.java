d7eeb52ee17f173712e598a5d8853ca21db76daa
Douglascrc <douglasbuscam@gmail.com>
2025-08-10 22:12:20 -0300

    feat: add Lombok @Getter annotation to Course entity

--- src/main/java/com/forumhub/domain/curso/Course.java
+++ src/main/java/com/forumhub/domain/curso/Course.java
@@ -3,10 +3,12 @@ package com.forumhub.domain.curso;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "Course")
 @Table(name = "courses")
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
